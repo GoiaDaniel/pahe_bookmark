7b3e0066931b82c1fad21d7223deb403a1d6dd98
MasterAbolis <danigoia3@gmail.com>
2021-12-28 16:42:05 +0200

    Added some rows to drawer; Moves drawer generation to functions;

--- drawer.js
+++ drawer.js
@@ -1,3 +1,31 @@
+//Create the Side nav element
+function createSideNav(storageResponseExample){
+  const sideNav = document.createElement('div')
+  sideNav.id = 'mySidenav'
+  sideNav.className = 'sidenav'
+  createSideNavMenu(sideNav,storageResponseExample);
+  closeButton(sideNav);
+  document.body.append(sideNav);
+}
+//Create close button for the sidenav
+function closeButton(sideNav){
+  const closeSideNav = document.createElement('span')
+  closeSideNav.innerText = 'CLOSE'
+  closeSideNav.className = 'closebtn'
+  closeSideNav.onclick = closeNav
+  sideNav.appendChild(closeSideNav)
+}
+//Create open button for the sidenav
+function openButton(){
+  const openSideNavButton = document.createElement('span')
+  openSideNavButton.id='openSideNavButton'
+  openSideNavButton.innerText = 'OPEN SIDENAV'
+  openSideNavButton.className = 'side-nav-open-btn'
+  openSideNavButton.onclick = openNav
+  document.body.append(openSideNavButton)
+}
+
+
 /*
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
@@ -16,31 +44,55 @@
 </div>
  */
 
-//SIDENAV
-const sideNav = document.createElement('div')
-sideNav.id = 'mySidenav'
-sideNav.className = 'sidenav'
+const storageResponseExample = [
+  {
+    title: 'jobless',
+    url: 'www.google.com',
+    watchedEpisodes : [1,2,3]
+  },
+  {
+    title: 'attack on titan',
+    url: 'www.google.com',
+    watchedEpisodes : [1,2,4]
+  }
+];
+function createWatchedEpisodesList(element,episodes){
+  let row = document.createElement('div');
+  row.style.display='block';
+  episodes.forEach( episode =>{
+    let pElement = document.createElement('a');
+    pElement.innerText = episode
+    pElement.href = 'www.google.com'
+    row.appendChild(pElement);
+  })
+  element.appendChild(row)
+}
 
-//SIDENAV MENU
-const sideNavMenu = document.createElement('span')
-sideNavMenu.innerText = 'TEST'
-sideNavMenu.onclick = sendMessage
-sideNav.appendChild(sideNavMenu)
+function createSideNavMenu(sideNav,storageResponseExample){
+  const sideNavMenu = document.createElement('a')
+  sideNavMenu.innerText = 'TEST'
+  sideNavMenu.onclick = sendMessage
+  sideNav.appendChild(sideNavMenu)
 
-const closeSideNav = document.createElement('span')
-closeSideNav.innerText = 'CLOSE'
-closeSideNav.className = 'closebtn'
-closeSideNav.onclick = closeNav
-sideNav.appendChild(closeSideNav)
+  storageResponseExample.forEach( title =>{
+    let element = document.createElement('a');
+    element.innerText = title.title;
+    element.href = title.url;
+    createWatchedEpisodesList(element,title.watchedEpisodes);
 
-//OPEN SIDENAV BUTTON
-const openSideNavButton = document.createElement('span')
-openSideNavButton.innerText = 'OPEN SIDENAV'
-openSideNavButton.className = 'side-nav-open-btn'
-openSideNavButton.onclick = openNav
+    sideNav.appendChild(element)
 
-document.body.append(sideNav)
-document.body.append(openSideNavButton)
+  });
+
+}
+
+
+createSideNav(storageResponseExample);
+
+openButton()
+
+
+
 
 console.log(document.body)
 
@@ -54,12 +106,14 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "60em";
+  document.getElementById("mySidenav").style.width = "30em";
+  document.getElementById('openSideNavButton').style.opacity = '0';
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById('openSideNavButton').style.opacity = '1';
 }
 
 
--- src/worker.js
+++ src/worker.js
@@ -1,17 +1,28 @@
 set('titles', [
   {
-    title: 'jobless'
+    title: 'jobless',
+    url: 'www.google.com',
+    watchedEpisodes : [1,2,3]
   },
   {
-    title: 'attack on titan'
+    title: 'attack on titan',
+    url: 'www.google.com',
+    watchedEpisodes : [1,2,4]
   }
 ])
 
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
 
 chrome.runtime.onMessage.addListener(async function(msg, sender){
   console.log(`EXTENSION`, msg)
   const titles = await get('titles')
   console.log(titles)
+  const tab = getCurrentTab();
+  console.log('Url: ',tab);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     console.log(`Sending message to content script`)
     chrome.tabs.sendMessage(tabs[0].id, titles, function(response) {
@@ -25,13 +36,15 @@ chrome.action.onClicked.addListener((tab) => {
   set('current_page', tab.url)
   console.log(tab)
 
-  chrome.scripting.executeScript({
+/*  chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: openDrawer
-  });
+  });*/
 
 
-  get('key')
+  get('key').then(res =>{
+    console.log('key?: ' ,res);
+  })
 });
 
 
